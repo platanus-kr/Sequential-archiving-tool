d28c8ace6466507c3a48657064b3afc982551ad1
platanus <platanus@canxan.com>
2020-02-02 18:44:08 +0900

    add display processing time and zipped time

--- sequarchtool.py
+++ sequarchtool.py
@@ -3,10 +3,11 @@ import zipfile
 import sys
 import logging
 import shutil
+import time
 
 from os import listdir, path
 from os.path import isfile, join, isdir
-from time import sleep, time
+from time import sleep
 from datetime import datetime
 
 import sequarchtool_config as cfg
@@ -33,38 +34,59 @@ def directory_zip(src_fullpath, dest_filename):
 		zip_.close()
 
 
-try:
-	os.path.isdir(cfg.destination_path)
-except:
-	logging_bucket(" >> Not found destination path.")
-	exit()
 
-try:
-	os.path.isdir(cfg.origin_path)
-except:
-	logging_bucket(" >> Not found origin path.")
-	exit()
+def convert_bytes(num):
+    for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
+        if num < 1024.0:
+            return "%3.1f %s" % (num, x)
+        num /= 1024.0
 
 
-try:
-	join_path_dsstore=os.path.join(cfg.origin_path,".DS_Store")
-	os.remove(join_path_dsstore)
-	logging_bucket(" >> .DS_Store removed.")
-except:
-	pass
+def file_size(file_path):
+    if os.path.isfile(file_path):
+        file_info = os.stat(file_path)
+        return convert_bytes(file_info.st_size)
 
-now = datetime.now()
-nowDatetime = now.strftime('%Y%m%d')
-# print(directory_list(cfg.origin_path))
 
-for dirname in directory_list(cfg.origin_path):
-	join_fullpath_path=os.path.join(cfg.origin_path, dirname)
-	join_fullpath_with_filename=os.path.join(cfg.origin_path, dirname+".zip")
-	join_destination_fullpath_with_filename=os.path.join(cfg.destination_path, dirname+".zip")
-	logging_bucket(" >> Archiving "+dirname+".")
-	directory_zip(join_fullpath_path, join_fullpath_with_filename)
-	logging_bucket(" >> Moving "+dirname+".zip. ")
-	shutil.move(join_fullpath_with_filename, join_destination_fullpath_with_filename)
-	logging_bucket(" >> Archiving and Moving OK "+dirname+".")
+if __name__ == "__main__":
+	try:
+		os.path.isdir(cfg.destination_path)
+	except:
+		logging_bucket(" >> Not found destination path.")
+		exit()
 
-logging_bucket(" >> Done Archiving.")
+	try:
+		os.path.isdir(cfg.origin_path)
+	except:
+		logging_bucket(" >> Not found origin path.")
+		exit()
+
+
+	try:
+		join_path_dsstore=os.path.join(cfg.origin_path,".DS_Store")
+		os.remove(join_path_dsstore)
+		logging_bucket(" >> .DS_Store removed.")
+	except:
+		pass
+
+	for dirname in directory_list(cfg.origin_path):
+		join_fullpath_path=os.path.join(cfg.origin_path, dirname)
+		join_fullpath_with_filename=os.path.join(cfg.origin_path, dirname+".zip")
+		join_destination_fullpath_with_filename=os.path.join(cfg.destination_path, dirname+".zip")
+		logging_bucket(" >> Archiving "+dirname+".")
+		arch_tic=time.process_time()
+		directory_zip(join_fullpath_path, join_fullpath_with_filename)
+		arch_toc=time.process_time()
+		# "{0:.2f}".format(a)
+		arch_proctime=str("{0:.2f}".format(arch_toc-arch_tic))
+		logging_bucket(" >> Archiving time : "+str(arch_proctime)+"s.")	
+		logging_bucket(" >> Archived file size :"+file_size(join_fullpath_with_filename)+".")
+		logging_bucket(" >> Moving "+dirname+".zip. ")
+		move_tic=time.process_time()
+		shutil.move(join_fullpath_with_filename, join_destination_fullpath_with_filename)
+		move_toc=time.process_time()
+		move_proctime=str("{0:.2f}".format(move_toc-move_tic))
+		logging_bucket(" >> Archiving and Moving OK "+dirname+".")
+		logging_bucket(" >> File trasfer time: "+str(move_proctime)+"s.")	
+
+	logging_bucket(" >> Done Archiving.")
