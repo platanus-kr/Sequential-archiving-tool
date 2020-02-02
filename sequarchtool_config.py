81e61d450c865edeca466f5e009d090f290d8b2e
platanus <platanus@canxan.com>
2020-02-02 19:06:10 +0900

    add configure comment

--- sequarchtool_config.py
+++ sequarchtool_config.py
@@ -1,2 +1,17 @@
+###
+# - 작성요렁
+# 
+# origin_path : 카탈로그가 들어있는 최상위 폴더의 경로를 지정합니다
+# destination_path : 압축하여 전송할 원격지를 지정합니다
+###
+# - Windows 사용자 경로 작성 유의사항
+# 윈도우 사용자의 경우 아래와 같이 변경하여 작성합니다
+#
+# 원래 : "D:\Catalog\Lightroom"
+# 변경 : "D://Catalog//Lightroom"
+#
+# 이외 macOS, Linux는 아래 원본을 참조하면 됩니다.
+###
+
 origin_path="/Users/platanus/Pictures/LightroomCatalog"
 destination_path="/Volumes/photo-catalog/LightroomCatalogBackup"
