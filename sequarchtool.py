c016e5381db8d5e4de8a05c14c7f095d67b2cc9f
platanus <platanus@canxan.com>
2021-01-12 22:40:13 +0900

    feat(sequarchtool): Change using zip to tarball

--- sequarchtool.py
+++ sequarchtool.py
@@ -1,36 +1,56 @@
-import os
-import zipfile
-import sys
 import logging
+import os
 import shutil
+import tarfile
 import time
-
-from os import listdir, path
-from os.path import isfile, join, isdir
-from time import sleep
+import zipfile
 from datetime import datetime
+from os import listdir
+from os.path import isdir
+
+import mgzip
 
 import sequarchtool_config as cfg
 
+
 def logging_bucket(messages):
-	logging.basicConfig(filename='sequarchtool.log', format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
-	logging.info('%s', messages)
-	print(str(datetime.now())+messages)
+    logging.basicConfig(filename='sequarchtool.log', format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',
+                        level=logging.INFO)
+    logging.info('%s', messages)
+    print(str(datetime.now()) + messages)
+
 
 def directory_list(path):
     res = [f for f in listdir(path) if isdir(path)]
     return res
 
-def directory_zip(src_fullpath, dest_filename):
-	with zipfile.ZipFile(dest_filename, 'w') as zip_:
-		rootpath = src_fullpath
-		for (paths, dir__, files) in os.walk(src_fullpath):
-			for f in files:
-				fullpath = os.path.join(paths, f)
-				relpath = os.path.relpath(fullpath, rootpath)
-				zip_.write(fullpath, relpath, zipfile.ZIP_DEFLATED)
-		zip_.close()
 
+def compress_zip(src_fullpath, dest_full_path_w_filename):
+    dest_full_path_w_filename += ".zip"
+    with zipfile.ZipFile(dest_full_path_w_filename, 'w') as zip_:
+        rootpath = src_fullpath
+        for (paths, dir__, files) in os.walk(src_fullpath):
+            for f in files:
+                fullpath = os.path.join(paths, f)
+                relpath = os.path.relpath(fullpath, rootpath)
+                zip_.write(fullpath, relpath, zipfile.ZIP_DEFLATED)
+        zip_.close()
+
+
+def archive_tarball(src_fullpath, dirname):
+    dest_filename = dirname + ".tar"
+    tar = tarfile.open(cfg.origin_path + "/" + dest_filename, "x:")
+    tar.add(src_fullpath, arcname=dirname)
+    tar.close()
+
+
+def compress_gzip(target_fullpath_w_filename):
+    output_fullpath_w_filename = target_fullpath_w_filename + ".gz"
+    target_filestream = open(target_fullpath_w_filename, "rb")
+    output_gz = mgzip.open(output_fullpath_w_filename, "wb", compresslevel=9)
+    data = target_filestream.read()
+    output_gz.write(data)
+    output_gz.close()
 
 
 def convert_bytes(num):
@@ -40,58 +60,70 @@ def convert_bytes(num):
         num /= 1024.0
 
 
-def file_size(file_path):
+def get_file_size(file_path) -> str:
     if os.path.isfile(file_path):
         file_info = os.stat(file_path)
-        return convert_bytes(file_info.st_size)
+        return str(convert_bytes(file_info.st_size))
 
 
 if __name__ == "__main__":
-	try:
-		os.path.isdir(cfg.destination_path)
-	except:
-		logging_bucket(" >> Not found destination path.")
-		exit()
+    if not os.path.isdir(cfg.destination_path):
+        logging_bucket(" >> Not found destination path.")
+        exit()
 
-	try:
-		os.path.isdir(cfg.origin_path)
-	except:
-		logging_bucket(" >> Not found origin path.")
-		exit()
+    if not os.path.isdir(cfg.origin_path):
+        logging_bucket(" >> Not found origin path.")
+        exit()
 
+    try:
+        join_path_dsstore = os.path.join(cfg.origin_path, ".DS_Store")
+        os.remove(join_path_dsstore)
+        logging_bucket(" >> .DS_Store removed.")
+    except:
+        pass
 
-	try:
-		join_path_dsstore=os.path.join(cfg.origin_path,".DS_Store")
-		os.remove(join_path_dsstore)
-		logging_bucket(" >> .DS_Store removed.")
-	except:
-		pass
+    for dirname in directory_list(cfg.origin_path):
+        filename_w_tar = dirname + ".tar"
+        filename_w_tar_gz = filename_w_tar + ".gz"
+        join_fullpath_path = os.path.join(cfg.origin_path, dirname)
+        # example) "/Volume/User/Catalog" + "Potrait" = "/Volume/User/Catalog/Portrait"
+        join_fullpath_w_tar_filename = os.path.join(cfg.origin_path, filename_w_tar)
+        # example) "/Volume/User/Catalog" + "Portrait" + ".tar" = "/Volume/User/Catalog/Portrait.tar"
+        join_fullpath_w_gz_filename = os.path.join(cfg.origin_path, filename_w_tar_gz)
+        # example) "/Volume/User/Catalog" + "Portrait" + ".tar.gz" = "/Volume/User/Catalog/Portrait.tar.gz"
+        join_destination_fullpath_w_tar_filename = os.path.join(cfg.destination_path, filename_w_tar)
+        # example) "/Volume/ExternalNAS/CatalogBackup" + "Portrait" + ".tar"
+        #
+        join_destination_fullpath_w_gz_filename = os.path.join(cfg.destination_path, filename_w_tar_gz)
+        # example) "/Volume/ExternalNAS/CatalogBackup" + "Portrait" + ".tar.gz"
+        #                                                       = "/Volume/ExternalNAS/CatalogBackup/Portrait.tar.gz"
 
-	for dirname in directory_list(cfg.origin_path):
-		join_fullpath_path=os.path.join(cfg.origin_path, dirname)
-		# example) "/Volume/User/Catalog" + "Potrait" = "/Volume/User/Catalog/Portrait"
-		join_fullpath_with_filename=os.path.join(cfg.origin_path, dirname+".zip")
-		# exanple) "/Volume/User/Catalog" + "Portrait" + ".zip" = "/Volume/User/Catalog/Portrait.zip"
-		join_destination_fullpath_with_filename=os.path.join(cfg.destination_path, dirname+".zip")
-		# exanple) "/Volume/ExternalNAS/CatalogBackup" + "Portrait" + ".zip" = "/Volume/ExternalNAS/CatalogBackup/Portrait.zip"
+        # Archive tarball
+        logging_bucket(" >> Archiving " + dirname + ".")
+        arch_tic = time.process_time()
+        # compress_zip(join_fullpath_path, join_fullpath_with_filename)
 
-		# Create zip file
-		logging_bucket(" >> Archiving "+dirname+".")
-		arch_tic=time.process_time()
-		directory_zip(join_fullpath_path, join_fullpath_with_filename)
-		arch_toc=time.process_time()
-		arch_proctime=str("{0:.2f}".format(arch_toc-arch_tic))
-		logging_bucket(" >> Archiving time : "+str(arch_proctime)+"s.")	
-		logging_bucket(" >> Archived file size :"+file_size(join_fullpath_with_filename)+".")
+        archive_tarball(join_fullpath_path, dirname)
+        file_size = get_file_size(join_fullpath_w_tar_filename)
+        logging_bucket(" >>>> Tarball size :" + str(file_size) + ".")
 
-		# Transfer to remote point
-		logging_bucket(" >> Moving "+dirname+".zip. ")
-		move_tic=time.process_time()
-		shutil.move(join_fullpath_with_filename, join_destination_fullpath_with_filename)
-		move_toc=time.process_time()
-		move_proctime=str("{0:.2f}".format(move_toc-move_tic))
-		logging_bucket(" >> File trasfer time: "+str(move_proctime)+"s.")	
+        # # Compress tarball to gzip
+        # compress_gzip(join_fullpath_w_tar_filename)
+        # os.remove(join_fullpath_w_tar_filename)
+        arch_toc = time.process_time()
+        # file_size = get_file_size(join_fullpath_w_gz_filename)
+        # logging_bucket(" >>>> Compressed file size :" + str(file_size) + ".")
+        arch_proctime = str("{0:.2f}".format(arch_toc - arch_tic))
+        logging_bucket(" >> Archiving time : " + str(arch_proctime) + "s.")
 
-		logging_bucket(" >> Archiving and Moving OK "+dirname+".")
+        # Transfer to remote point
+        # logging_bucket(" >> Moving " + filename_w_tar_gz)
+        logging_bucket(" >> Moving " + filename_w_tar)
+        move_tic = time.process_time()
+        shutil.move(join_fullpath_w_tar_filename, join_destination_fullpath_w_tar_filename)
+        # shutil.move(join_fullpath_w_gz_filename, join_destination_fullpath_w_gz_filename)
+        move_toc = time.process_time()
+        move_proctime = str("{0:.2f}".format(move_toc - move_tic))
+        logging_bucket(" >> File trasfer time: " + str(move_proctime) + "s.")
 
-	logging_bucket(" >> Done Archiving.")
+    logging_bucket(" >> Done Archiving.")
